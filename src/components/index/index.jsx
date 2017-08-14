e6e0f65cb8b5fc4f8f44a9118a3783ced6a595e2
吉祥 <wb-257126@alibaba-inc.com>
2017-08-14 17:13:02 +0800

    :tada:

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -1,15 +1,36 @@
+/**
+ * @name: 首页
+ */
+
 import React, { Component } from 'react';
+import { Tabs } from 'antd-mobile';
+import 'whatwg-fetch';
+import NEWS_API from '../../constants/index.js';
 import './style.scss';
+
+const TabPane = Tabs.TabPane;
 
 export default class extends Component {
   constructor() {
     super();
     this.state = {};
   }
+  componentDidMount() {
+    fetch(NEWS_API.list).then((data) => {
+      console.log(data);
+    });
+  }
   render() {
     return (
       <div className="index">
-        123
+        <Tabs animated={false} swipeable={false}>
+          <TabPane tab="First tab" key="1">123123</TabPane>
+          <TabPane tab="second tab" key="2">123123</TabPane>
+          <TabPane tab="third tab" key="3">123123</TabPane>
+          <TabPane tab="four tab" key="4">123123</TabPane>
+          <TabPane tab="five tab" key="5">123123</TabPane>
+          <TabPane tab="six tab" key="6">123123</TabPane>
+        </Tabs>
       </div>
     );
   }
---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+/**
+ * @name: 一些常量
+ */
+
+// const domain = 'http://mimikj.com:6060';
+//
+// const NEWS_API = {
+//   list: domain + '/mimikj/trigger/infor_flow_td/list',
+// };
+
+export default { a: 1 };
